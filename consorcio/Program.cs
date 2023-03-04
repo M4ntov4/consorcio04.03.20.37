7b1ea4994d3785c8b3dcafb1b344ceeb15fb8391
senho <senho@SERVER>
2023-03-04 20:37:55 -0300

    Adicionar arquivos de projeto.

---
+++ consorcio/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consorcio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int participantes;
+            int j = 0;
+
+
+            Console.WriteLine("insira o número de participantes: ");
+            participantes = int.Parse(Console.ReadLine());
+
+            string[,] dados = new string[participantes, participantes];
+
+            for (int i = 0; i < participantes; i++)
+            {
+
+
+                
+
+                Console.Write("Digite o nome do cliente para a posição [{0},{1}]: ", i, j);
+                string nomeCliente = Console.ReadLine();
+
+                Console.Write("Digite o número para o cliente {0}: ", nomeCliente);
+                int numeroCliente = Convert.ToInt32(Console.ReadLine());
+
+                dados[i, j] = $"{nomeCliente} ({numeroCliente})";
+                j++;
+
+
+
+
+
+            }
+
+
+
+            //for (int i = 0; i < participantes; i++)
+           // {
+                
+             //       Console.WriteLine(dados[i,i]);
+                
+           // }
+
+            Console.ReadKey();
+
+        }
+    }
+}
